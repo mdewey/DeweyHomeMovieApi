c3641e7c863b14aab4c7a49c8d67fc253e659b48
Mark Dewey <mtdewey55@gmail.com>
2022-10-23 10:12:21 -0400

    (feature) moved swagger path

--- Program.cs
+++ Program.cs
@@ -37,7 +37,10 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+  c.RoutePrefix = "";
+});
 
 
 app.UseCors("video-sites");
